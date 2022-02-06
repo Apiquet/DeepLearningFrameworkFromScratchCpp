6907713cb7cf6ad11adb9b0abf795111c644ee54
Anthony <anthony.piquet2@gmail.com>
2022-02-06 23:51:02 +0100

    [DEV][TRAIN] Add trainer definition

---
+++ include/Trainer.hpp
@@ -0,0 +1,18 @@
+/**
+ * Trainer class definition
+*/
+
+#pragma once
+
+namespace DeepLearningFramework
+{
+    class Trainer
+    {
+        public:
+            Trainer();
+            ~Trainer() = default;
+
+            /* Print description of trainer class */
+            void printDescription();
+    };    
+}; // namespace DeepLearningFramework
