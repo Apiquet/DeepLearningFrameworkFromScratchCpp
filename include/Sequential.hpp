ffad37b96cb9b325e0032065d2543be4a3f16d58
Anthony <anthony.piquet2@gmail.com>
2022-02-07 23:17:16 +0100

    [DEV][SEQ] Add docs and struct

--- include/Sequential.hpp
+++ include/Sequential.hpp
@@ -8,17 +8,51 @@
 
 namespace DeepLearningFramework
 {
+    /**
+     * Sequential class.
+     * 
+     * Class used to store in sequence multiple modules to create a neural network
+     * 
+     * forward: apply forward pass for each module in sequence
+     * backward: calculate loss and apply backward pass for each layer in reverse order.
+     */
     class Sequential: public Module
     {
         public:
             Sequential();
             ~Sequential() = default;
 
+            /**
+             * Apply forward pass for each layer in sequence.
+             *
+             * @param inputData Values on which to apply all layers in sequence.
+             * @return neural network result
+             */
             void forward();
 
+            /**
+             * Calculate loss and apply backward pass for each layer in reverse order.
+             *
+             * @param y target results.
+             * @param yPred obtained results from the neural network.
+             * @return loss value
+             */
             void backward();
 
-            /* Print description of linear layer class */
+            /* Print description of each module in sequence */
             void printDescription();
+
+            /**
+             * Set learning rate used to update weights and bias.
+             *
+             * @param lr learning rate to use.
+             */
+            void setLR();
+
+            /** Get the number of parameters of the model. */
+            void getParametersCount();
+
+        private:
+            // type, name, neural network
     };    
 }; // namespace DeepLearningFramework
