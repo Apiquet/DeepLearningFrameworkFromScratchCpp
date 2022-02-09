1f6393fc7eaefcde3fd63cc12e918ca6b06df039
Anthony <anthony.piquet2@gmail.com>
2022-02-10 00:01:24 +0100

    [DEV][SOFT][TEST] Add forward backward tests

--- unitTests/SoftmaxUnitTests.cpp
+++ unitTests/SoftmaxUnitTests.cpp
@@ -2,9 +2,9 @@
 
 using namespace DeepLearningFramework;
 
-void testEquation(Activations::Softmax& softmaxActivation)
+void testForwardPass(Activations::Softmax& softmaxActivation)
 {
-    std::cout << "Equation test:" << std::endl;
+    std::cout << "Forward test:" << std::endl;
 
     Eigen::MatrixXf x {
       {0.f, 1.f, 0.f},
@@ -19,7 +19,7 @@ void testEquation(Activations::Softmax& softmaxActivation)
     };
 
     Eigen::MatrixXf out;
-    softmaxActivation.equation(out, x);
+    softmaxActivation.forward(out, x);
 
     if(target.isApprox(out))
         std::cout << "OK" << std::endl;
@@ -27,19 +27,55 @@ void testEquation(Activations::Softmax& softmaxActivation)
         std::cout << "KO" << std::endl;
 }
 
-void testForwardPass(Activations::Softmax& softmaxActivation)
-{
-}
-
 void testBackwardPass(Activations::Softmax& softmaxActivation)
 {
+    std::cout << "Backward test:" << std::endl;
+
+    Eigen::MatrixXf inputForward {
+      {0.f, 1.f, 0.f},
+      {2.f, 0.f, 1.f},
+      {0.f, 3.f, 3.f},
+    };
+
+    Eigen::MatrixXf targetForward {
+      {0.211942f,  0.576117f,  0.211942f},
+      {0.665241f,  0.0900306f, 0.244728},
+      {0.0242889f, 0.487856f,  0.487856},
+    };
+
+    Eigen::MatrixXf out;
+    softmaxActivation.forward(out, inputForward);
+
+    if(targetForward.isApprox(out))
+    {
+        Eigen::MatrixXf inputBackward {
+            {0.f, 1.f, 0.f},
+            {2.f, 0.f, 1.f},
+            {0.f, 3.f, 3.f},
+        };
+
+        Eigen::MatrixXf targetBackward {
+            {0.f,      0.722632f, 0.f},
+            {1.58634f, 0.f,       0.717583f},
+            {0.f,      2.11225f,  1.91237f},
+        };
+
+        Eigen::MatrixXf out;
+        softmaxActivation.backward(out, inputBackward);
+
+        if(targetBackward.isApprox(out))
+            std::cout << "OK" << std::endl;
+        else
+            std::cout << "KO" << std::endl;
+    }
+    else
+        std::cout << "KO Forward" << std::endl;
 }
 
 int main()
 {
     std::cout << "Softmax activation unit tests" << std::endl;
     Activations::Softmax softmaxActivation;
-    testEquation(softmaxActivation);
-    // testForwardPass(softmaxActivation);
-    // testBackwardPass(softmaxActivation);
+    testForwardPass(softmaxActivation);
+    testBackwardPass(softmaxActivation);
 }
