e2b09c509b6524de75bff05713b3db2d3d9ae33b
Anthony <anthony.piquet2@gmail.com>
2022-02-10 22:51:32 +0100

    [DEV][SEQ][TEST] Forward backward simple test 4x4

--- test/main.cpp
+++ test/main.cpp
@@ -9,11 +9,11 @@ using namespace DeepLearningFramework;
 int main()
 {
     std::vector<Module*> model;
-    model.emplace_back(new Layers::Linear((int)2, (int)50));
+    model.emplace_back(new Layers::Linear((int)4, (int)4));
     model.emplace_back(new Activations::ReLU());
-    model.emplace_back(new Layers::Linear((int)50, (int)50));
+    model.emplace_back(new Layers::Linear((int)4, (int)4));
     model.emplace_back(new Activations::ReLU());
-    model.emplace_back(new Layers::Linear((int)50, (int)2));
+    model.emplace_back(new Layers::Linear((int)4, (int)4));
     model.emplace_back(new Activations::Softmax());
     
     Losses::MSE mseLoss;
@@ -22,4 +22,24 @@ int main()
 
     sequential.printDescription();
     sequential.setLR(0.1);
+
+    Eigen::MatrixXf x {
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+    };
+    sequential.forward(x);
+    std::cout << "Model output: " << x << std::endl;
+
+    float loss = 0;
+    Eigen::MatrixXf y {
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+      {-9.f, -5.f, 4.f, 2.f},
+    };
+    sequential.backward(loss, y, x);
+    std::cout << "Loss: " << loss << std::endl;
+    std::cout << "Final backward: " << x << std::endl;
 }
