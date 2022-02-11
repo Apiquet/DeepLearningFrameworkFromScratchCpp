0ff6f6c25143b4198aabef053ed2cf84397a2f59
Anthony <anthony.piquet2@gmail.com>
2022-02-12 00:37:55 +0100

    [DEV][SOFTMAX][TEST] Update unit tests

--- unitTests/SoftmaxUnitTests.cpp
+++ unitTests/SoftmaxUnitTests.cpp
@@ -2,9 +2,11 @@
 
 using namespace DeepLearningFramework;
 
-void testForwardPass(Activations::Softmax& softmaxActivation)
+void softmaxActivationForwardPassTest()
 {
     std::cout << "Forward test:" << std::endl;
+
+    Activations::Softmax softmaxActivation;
 
     Eigen::MatrixXf x {
       {0.f, 1.f, 0.f},
@@ -21,15 +23,22 @@ void testForwardPass(Activations::Softmax& softmaxActivation)
     Eigen::MatrixXf out;
     softmaxActivation.forward(out, x);
 
-    if(target.isApprox(out))
-        std::cout << "OK" << std::endl;
-    else
-        std::cout << "KO" << std::endl;
+    if(!target.isApprox(out))
+    {
+        std::cout << "Result KO" << std::endl;
+        std::cout << "Expect: " << target << std::endl;
+        std::cout << "Got: " << out << std::endl;
+        return;
+    }
+
+    std::cout << "OK" << std::endl;
 }
 
-void testBackwardPass(Activations::Softmax& softmaxActivation)
+void softmaxActivationBackwardPassTest()
 {
     std::cout << "Backward test:" << std::endl;
+
+    Activations::Softmax softmaxActivation;
 
     Eigen::MatrixXf inputForward {
       {0.f, 1.f, 0.f},
@@ -37,45 +46,30 @@ void testBackwardPass(Activations::Softmax& softmaxActivation)
       {0.f, 3.f, 3.f},
     };
 
-    Eigen::MatrixXf targetForward {
-      {0.211942f,  0.576117f,  0.211942f},
-      {0.665241f,  0.0900306f, 0.244728},
-      {0.0242889f, 0.487856f,  0.487856},
-    };
-
     Eigen::MatrixXf out;
     softmaxActivation.forward(out, inputForward);
 
-    if(targetForward.isApprox(out))
+    Eigen::MatrixXf inputBackward {
+        {0.f, 1.f, 0.f},
+        {2.f, 0.f, 1.f},
+        {0.f, 3.f, 3.f},
+    };
+
+    Eigen::MatrixXf targetBackward {
+        {0.f,      0.722632f, 0.f},
+        {1.58634f, 0.f,       0.717583f},
+        {0.f,      2.11225f,  1.91237f},
+    };
+
+    softmaxActivation.backward(out, inputBackward);
+
+    if(!targetBackward.isApprox(out))
     {
-        Eigen::MatrixXf inputBackward {
-            {0.f, 1.f, 0.f},
-            {2.f, 0.f, 1.f},
-            {0.f, 3.f, 3.f},
-        };
-
-        Eigen::MatrixXf targetBackward {
-            {0.f,      0.722632f, 0.f},
-            {1.58634f, 0.f,       0.717583f},
-            {0.f,      2.11225f,  1.91237f},
-        };
-
-        Eigen::MatrixXf out;
-        softmaxActivation.backward(out, inputBackward);
-
-        if(targetBackward.isApprox(out))
-            std::cout << "OK" << std::endl;
-        else
-            std::cout << "KO" << std::endl;
+        std::cout << "Result KO" << std::endl;
+        std::cout << "Expect: " << targetBackward << std::endl;
+        std::cout << "Got: " << out << std::endl;
+        return;
     }
-    else
-        std::cout << "KO Forward" << std::endl;
-}
 
-int main()
-{
-    std::cout << "Softmax activation unit tests" << std::endl;
-    Activations::Softmax softmaxActivation;
-    testForwardPass(softmaxActivation);
-    testBackwardPass(softmaxActivation);
+    std::cout << "OK" << std::endl;
 }
