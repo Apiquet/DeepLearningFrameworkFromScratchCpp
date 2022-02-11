220015d19c443591c63bf7f63247d021d7ac3cf1
Anthony <anthony.piquet2@gmail.com>
2022-02-12 00:38:16 +0100

    [DEV][TEST] Script run all unit tests

---
+++ unitTests/runAllTests.cpp
@@ -0,0 +1,25 @@
+#include "LinearUnitTests.cpp"
+#include "MSEUnitTests.cpp"
+#include "ReLUUnitTests.cpp"
+#include "SoftmaxUnitTests.cpp"
+
+using namespace DeepLearningFramework;
+
+int main()
+{
+    std::cout << "Linear layer unit tests" << std::endl;
+    linearLayerForwardPassTest();
+    linearLayerBackwardPassTest();
+
+    std::cout << "MSE loss unit tests" << std::endl;
+    mseLossForwardPassTest();
+    mseLossBackwardPassTest();
+
+    std::cout << "ReLU activation unit tests" << std::endl;
+    reluActivationForwardPassTest();
+    reluActivationBackwardPassTest();
+
+    std::cout << "Softmax activation unit tests" << std::endl;
+    softmaxActivationForwardPassTest();
+    softmaxActivationBackwardPassTest();
+}
